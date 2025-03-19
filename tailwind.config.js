e5719c1f0536054a7533ba0b0c5ec63b0cda3a73
Pranav-Sandeep-Shinde <ss36507703@gmail.com>
2025-03-19 20:55:36 +0530

    Some minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"], // Ensure the correct path
   safelist: [
     { pattern: /^bg-(teal|sky)-(100|200|300|400|500|600|700|800|900)$/ }, // Background colors
-    { pattern: /^hover:bg-(teal|sky)-(600|700)$/ }, // Hover backgrounds
+    // { pattern: /^hover:bg-(teal|sky)-(600|700)$/ }, // Hover backgrounds
     { pattern: /^text-(teal|sky)-(600|700)$/ }, // Text colors
     { pattern: /^border-(teal|sky)-(600|700)$/ }, // Border colors
   ],
