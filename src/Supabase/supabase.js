728e52573c3a7f851b5fec4fc0162a961d3f1100
Parimal Matte <123257338+PmCoder26@users.noreply.github.com>
2025-03-31 10:11:28 +0530

    supabase commit

--- src/Supabase/supabase.js
+++ src/Supabase/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://njuuiiwafdfbsyyzdowj.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qdXVpaXdhZmRmYnN5eXpkb3dqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUxOTAyOTAsImV4cCI6MjA1MDc2NjI5MH0.iysWrx9ikAnScIgne5U6X3l9QoH-94CSVCotZcEuvEE'
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
